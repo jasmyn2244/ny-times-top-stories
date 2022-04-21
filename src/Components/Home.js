cd2b9b1e52e0f8a68b9f03cdf954cbee7b40230e
Jasmyn Hicks <jasmynh44@gmail.com>
2022-04-21 14:34:59 -0600

    Add Home component with nested SectionButtons component

---
+++ src/Components/Home.js
@@ -0,0 +1,11 @@
+import { render } from '@testing-library/react'
+import React from 'react'
+import SectionButtons from './SectionButtons'
+
+const Home = () => {
+  return (
+    <SectionButtons />
+  )
+}
+
+export default Home 
---
+++ src/Components/SectionButtons.js
@@ -0,0 +1,19 @@
+import React from 'react'
+
+const SectionButtons = () => {
+  return (
+    <div>
+      <button>Arts</button>
+      <button>Books</button>
+      <button>Business</button>
+      <button>Health</button>
+      <button>Politics</button>
+      <button>Science</button>
+      <button>Sports</button>
+      <button>Technology</button>
+      <button>Travel</button>
+      <button>World</button>    </div>
+  )
+}
+
+export default SectionButtons
