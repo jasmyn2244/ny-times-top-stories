f55037d79c3355b5471f705686fada431a952ad5
Jasmyn Hicks <jasmynh44@gmail.com>
2022-04-21 14:51:40 -0600

    Add Home route to App.js

--- src/App.js
+++ src/App.js
@@ -1,15 +1,14 @@
-import logo from './logo.svg';
 import './App.css';
 import { Routes, Route } from 'react-router-dom'
 import Header from './Components/Header'
+import Home from './Components/Home'
 
 function App() {
   return (
     <>
       <Header />
       <Routes>
-
-
+        <Route path='/' element={<Home />} />
       </Routes>
     </>
   );
